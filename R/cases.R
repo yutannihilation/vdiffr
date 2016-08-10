efd60d2f9b9a4c4c8117948ed10d3de7b1eb2f29
Lionel Henry <lionel.hry@gmail.com>
2016-08-10 09:24:13 +0200

    Give feedback when testthat is run

--- R/cases.R
+++ R/cases.R
@@ -17,6 +17,7 @@
 collect_cases <- function(package = ".") {
   on.exit(set_active_collecter(NULL))
 
+  cat("Running testthat to collect visual cases\n")
   package <- devtools::as.package(package)
   reporter <- vdiffrReporter$new(package$path)
   devtools::test(package, reporter = reporter)
