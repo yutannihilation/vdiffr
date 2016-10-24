e4e12eaf911026341b3f55518abbbc16ca827ad6
Lionel Henry <lionel.hry@gmail.com>
2016-10-24 17:24:41 +0200

    Add Base graphics test

--- tests/testthat/mock-pkg/tests/testthat/test-passed.R
+++ tests/testthat/mock-pkg/tests/testthat/test-passed.R
@@ -2,8 +2,12 @@
 context("Passed plots")
 library("vdiffr")
 
-p1_orig <- ggplot2::ggplot(mtcars, ggplot2::aes(disp)) + ggplot2::geom_histogram()
-
-test_that("doppelgangers pass", {
+test_that("ggplot doppelgangers pass", {
+  p1_orig <- ggplot2::ggplot(mtcars, ggplot2::aes(disp)) + ggplot2::geom_histogram()
   expect_doppelganger("myplot", p1_orig, "")
+})
+
+test_that("base doppelgangers pass", {
+  p2_orig <- function() plot(mtcars$disp)
+  expect_doppelganger("myplot2", p2_orig, "")
 })
