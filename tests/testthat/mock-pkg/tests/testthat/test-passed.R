bc8f13f761833ed31947a657544c411b84ba9293
Lionel Henry <lionel.hry@gmail.com>
2016-10-24 21:54:30 +0200

    Add grid test

--- tests/testthat/mock-pkg/tests/testthat/test-passed.R
+++ tests/testthat/mock-pkg/tests/testthat/test-passed.R
@@ -11,3 +11,13 @@ test_that("base doppelgangers pass", {
   p2_orig <- function() plot(mtcars$disp)
   expect_doppelganger("myplot2", p2_orig, "")
 })
+
+library("grid")
+test_that("grid doppelgangers pass", {
+  p_grid <- function() {
+    grid.newpage()
+    grid.text("foobar", gp = gpar(fontsize = 10.1))
+    grid.text("foobaz", 0.5, 0.1, gp = gpar(fontsize = 15.05))
+  }
+  expect_doppelganger("Grid doppelganger", p_grid)
+})
