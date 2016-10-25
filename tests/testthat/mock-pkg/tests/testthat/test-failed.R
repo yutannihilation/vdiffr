f5eb499537955acadcdea8e2b5067bb751fa27f0
Lionel Henry <lionel.hry@gmail.com>
2016-10-25 11:10:59 +0200

    Update tests to latest svglite

--- tests/testthat/mock-pkg/tests/testthat/test-failed.R
+++ tests/testthat/mock-pkg/tests/testthat/test-failed.R
@@ -5,6 +5,10 @@ library("vdiffr")
 p1_orig <- ggplot2::ggplot(mtcars, ggplot2::aes(disp)) + ggplot2::geom_histogram()
 p1_fail <- p1_orig + ggplot2::geom_vline(xintercept = 300)
 
+test_that("(maintenance) Reset failing figure", {
+  skip("maintenance")
+  expect_doppelganger("myplot", p1_orig, "")
+})
 test_that("New plots work are collected", {
   expect_doppelganger("myplot", p1_fail, "")
 })
