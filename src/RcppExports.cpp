2037933d73523f9772138bf09c98ce91cb13d809
Lionel Henry <lionel.hry@gmail.com>
2017-07-31 11:57:20 +0200

    Use new Rcpp registration system

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,7 +7,7 @@ using namespace Rcpp;
 
 // compare_files
 bool compare_files(std::string expected, std::string test);
-RcppExport SEXP vdiffr_compare_files(SEXP expectedSEXP, SEXP testSEXP) {
+RcppExport SEXP _vdiffr_compare_files(SEXP expectedSEXP, SEXP testSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -16,4 +16,14 @@ BEGIN_RCPP
     rcpp_result_gen = Rcpp::wrap(compare_files(expected, test));
     return rcpp_result_gen;
 END_RCPP
+}
+
+static const R_CallMethodDef CallEntries[] = {
+    {"_vdiffr_compare_files", (DL_FUNC) &_vdiffr_compare_files, 2},
+    {NULL, NULL, 0}
+};
+
+RcppExport void R_init_vdiffr(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
 }
