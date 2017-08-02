5da192293dea88ee0fc1bdd4a6585eef60df1953
Lionel Henry <lionel.hry@gmail.com>
2017-08-02 20:40:46 +0200

    Skip vdiffr tests when FreeType version doesn't match

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,16 @@
-library("testthat")
-library("vdiffr")
 
-test_check("vdiffr")
+mock_path <- file.path("testthat", "mock-pkg", "tests")
+cases_ver <- vdiffr:::cases_freetype_version(mock_path)
+system_ver <- vdiffr:::system_freetype_version()
+
+if (cases_ver != system_ver) {
+  cat(glue::glue(
+    "Skipping tests because the mock package was generated with a different FreeType version
+     Mock package: { cases_ver }
+     System: { system_ver }\n"
+  ))
+} else {
+  library("testthat")
+  library("vdiffr")
+  test_check("vdiffr")
+}
