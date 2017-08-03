cdb872570636c73475a0e72d5d3e3d83e96cf59a
Lionel Henry <lionel.hry@gmail.com>
2017-08-03 10:03:22 +0200

    Import something from Rcpp namespace
    To avoid a NOTE on some systems

--- R/vdiffr.R
+++ R/vdiffr.R
@@ -2,6 +2,7 @@
 #' @importFrom glue glue
 #' @importFrom purrr map map_chr keep walk every partial map2_chr
 #' @importFrom R6 R6Class
+#' @importFrom Rcpp sourceCpp
 #' @useDynLib vdiffr, .registration = TRUE
 "_PACKAGE"
 
