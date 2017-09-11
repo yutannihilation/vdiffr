097e7fdc54bc7a543dab6c7dd97553eb83ee9895
Lionel Henry <lionel.hry@gmail.com>
2017-09-11 19:06:24 +0200

    Update mock package visual tests to FreeType 2.8.0

--- tests/testthat/mock-pkg/tests/testthat/test-failed.R
+++ tests/testthat/mock-pkg/tests/testthat/test-failed.R
@@ -5,18 +5,30 @@ library("vdiffr")
 p1_orig <- ggplot2::ggplot(mtcars, ggplot2::aes(disp)) + ggplot2::geom_histogram()
 p1_fail <- p1_orig + ggplot2::geom_vline(xintercept = 300)
 
+maintenance <- FALSE
+skip_if_maintenance <- function() {
+  if (maintenance) {
+    skip("maintenance")
+  }
+}
+
 test_that("(maintenance) Reset failing figure", {
-  skip("maintenance")
+  if (!maintenance) {
+    skip("maintenance")
+  }
   expect_doppelganger("myplot", p1_orig, "")
 })
 test_that("New plots work are collected", {
+  skip_if_maintenance()
   expect_doppelganger("myplot", p1_fail, "")
 })
 
 test_that("Duplicated expectations issue a warning", {
+  skip_if_maintenance()
   expect_doppelganger("myplot", p1_fail, "")
 })
 
 test_that("SVGs of failing cases are printed when `verbose` is TRUE", {
+  skip_if_maintenance()
   expect_doppelganger("myplot", p1_fail, "", verbose = TRUE)
 })
