95fcab63436063ccd0aa61652d3aa8aea4ce6ae5
Lionel Henry <lionel.hry@gmail.com>
2018-01-31 04:41:13 -0800

    Fix ggplot scoping error in tests

--- tests/testthat/mock-pkg-skip-older/tests/testthat/test-skip-newer.R
+++ tests/testthat/mock-pkg-skip-older/tests/testthat/test-skip-newer.R
@@ -1,7 +1,7 @@
 context("Failing test")
 
-original <- ggplot(mtcars)
-failing <- ggplot(mtcars) + geom_vline(xintercept = 0)
+original <- ggplot2::ggplot(mtcars)
+failing <- ggplot2::ggplot(mtcars) + ggplot2::geom_vline(xintercept = 0)
 
 test_that("this fails", {
   expect_doppelganger("failing", failing)
