4384feb94141fb3f69ea24c7186f26bdb63cbcd5
Nate <nathancday@gmail.com>
2019-09-01 14:35:10 -0400

    Use up/down arrows to switch type

--- inst/www/toggle.js
+++ inst/www/toggle.js
@@ -18,11 +18,7 @@ function toggleBtns(message) {
 //Keyboard shortcuts
 document.onkeydown = function(e) {
   if (e.shiftKey) { // using e.shiftKey && e.which == 86 simultaneously doesn't work?
-    if (e.which == 39) { // down-arrow
-      Shiny.onInputChange("nextType", Math.random());
-    } else if (e.which == 37) { //up-arrow
-      Shiny.onInputChange("prevType", Math.random());
-    } else if (e.which == 86) { // v
+    if (e.which == 13) { // ENTER
       Shiny.onInputChange("validateGroup", Math.random());
     }
   } else {
@@ -30,7 +26,11 @@ document.onkeydown = function(e) {
       Shiny.onInputChange("nextCase", Math.random());
      } else if (e.which == 37) { // left-arrow
       Shiny.onInputChange("prevCase", Math.random());
-     } else if (e.which == 86) { // v
+     } else if (e.which == 40) { // down-arrow
+      Shiny.onInputChange("nextType", Math.random());
+     } else if (e.which == 38) { //up-arrow
+      Shiny.onInputChange("prevType", Math.random());
+     } else if (e.which == 13) { // ENTER
       Shiny.onInputChange("validateCase", Math.random());
     } else if (e.which == 27) { //esc, q = 81
       Shiny.onInputChange("quit_button", Math.random());
